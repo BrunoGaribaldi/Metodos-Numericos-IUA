772aa2aa26a9947054a66861778f8f4c8a9e52ef
Bruno <115671955+BrunoGaribaldi@users.noreply.github.com>
2023-11-14 09:12:56 -0300

    Add files via upload

---
+++ FileWriterExamplle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	ofstream archivo("Data.txt");
+	int a = 9,b=3;
+	archivo<<a<<"\t"<<b<<endl;
+	archivo<<a<<"\t"<<b<<endl;
+	return 0;
+}
+
